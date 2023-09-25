04df7710e6b9af030d45e9bd6c942c31ed442886
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2023-09-24 23:02:58 -0300

    🚑

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,18 @@ import cors from "cors";
 
 const app = express();
 
-
 app.use(express.json());
 app.use(router);
-app.use(cors({origin: 'http://127.0.0.1:5173/'}));
+app.use(cors());
 
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
