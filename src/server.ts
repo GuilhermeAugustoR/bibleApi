d99bc373bc729932371d7509b16bcb2024a15eb0
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2023-09-24 22:58:20 -0300

    🚑

--- src/server.ts
+++ src/server.ts
@@ -3,30 +3,13 @@ import { router } from "./routes";
 import cors from "cors";
 
 const app = express();
-app.use(cors())
 
 
 app.use(express.json());
 app.use(router);
+app.use(cors({origin: 'http://127.0.0.1:5173/'}));
 
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
