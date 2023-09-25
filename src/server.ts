d8f25d8618e126b9e45e51df1911e8c91652e301
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2023-09-24 23:09:24 -0300

    🚑

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(router);
 
 var corsOptions = {
-  origin: "https://bible-api-nv.vercel.app",
+  origin: "http://127.0.0.1:5173/",
 };
 app.use(cors(corsOptions));
 
