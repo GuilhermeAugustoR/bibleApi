5cb53bc82de3edff61a71d838b1fdfe3c7b2f781
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2026-02-14 16:20:39 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,20 @@ import { router } from "./routes";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 app.use(router);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 const PORT = process.env.PORT || 3333;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
